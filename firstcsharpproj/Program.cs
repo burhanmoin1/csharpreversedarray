30d3e549f15801e6c58d2310aec52ccf4ed46b2d
Burhan <m.burhanmoin1@gmail.com>
2024-04-24 17:41:14 +0500

    Add project files.

---
+++ firstcsharpproj/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] num = { 1, 2, 3, 4, 5 };
+        Array.Reverse(num);
+        int[] reversednumarray = new int[num.Length];
+        for (int i = 0; i < num.Length; i++)
+        {
+            reversednumarray[i] = num[i];
+        }
+
+        foreach (int reversenum in reversednumarray)
+        {
+            Console.WriteLine(reversenum);
+        }
+    }
+}
